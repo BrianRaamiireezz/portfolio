1adab4230311d0ca332fe50496e55ea30e456bf0
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2025-05-16 15:59:52 -0600

    Add: Path alias for the /src/components/ folder - Update package.json to add the @types/node dev dependency used in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+
+import path from 'path';
 
 
 export default defineConfig(
     {
         plugins: [ react() ],
+        resolve: {
+            alias: {
+                '@components': path.resolve( './src/components' ),
+            },
+        },
     },
 );
