cb2ffb4a6fafb9d6575b77c0dbe0c7d77000b0ca
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2025-05-17 17:15:54 -0600

    Add: Project assets and data

---
+++ src/assets/data/projects/index.ts
@@ -0,0 +1,24 @@
+export const Projects = [
+    {
+        to: 'IBMJavaScriptProgrammingEssentials',
+        name: '[IBM] JavaScript Programming Essentials',
+        tags: [ 'coursera' ],
+        hue: '214',
+
+        image: {
+            source: 'drtravel.png',
+            alt: 'The website root page',
+        },
+        description: `A fictional travel recommmendation site. The project foucuses on the use of javascript to generate HTML content dinamically based on the destinations the user searches for. The available places are beaches, countries, and temples.`,
+        links: [
+            {
+                name: 'visit',
+                to: 'https://brianraamiireezz.github.io/Coursera-JavaScriptEssentialsIBM-FinalProject/',
+            },
+            {
+                name: 'certificate',
+                to: 'https://coursera.org/share/0b1e0925b519e5fcfddc116d45bc4dab',
+            },
+        ],
+    },
+];
